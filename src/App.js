5ea0e11300e4ab9b534d6afa078f43167aed7d8a
karenbarseghyan90@gmail.com <karenbarseghyan90@gmail.com>
2021-12-13 16:25:55 +0400

    Creating redux-store & providing it to React-App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import CakeContainer from "./components/CakeContainer";
+
 
 function App() {
   return (
-    <div>
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+       <div>
+        <CakeContainer />
+      </div>
+    </Provider>
+   
   );
 }
 
